850ed40444f17065f67fcedae7be82875754fe95
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-20 20:20:57 +0530

    Added:Find the Cheapest One

--- src/HotelReservationMain/HotelReservationTest.java
+++ src/HotelReservationMain/HotelReservationTest.java
@@ -1,11 +1,13 @@
 package HotelReservationMain;
 
 import org.junit.Test;
-
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class HotelReservationTest {
     @Test
@@ -22,5 +24,19 @@ public class HotelReservationTest {
         List<Hotel> result = hotelReservation.getHotel();
         assertEquals(hotels, result);
     }
+    public void whenGivenDateRangeShouldReturnCheapestHotel()
+    {
+        Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.add(hotel1);
+        hotelReservation.add(hotel2);
+        hotelReservation.add(hotel3);
+        Map<Integer, Hotel> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
+        result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
+        assertNotNull(result);
+    }
 }
+
 
