41a131ac66959b7cc685dcf34212cbbc594986d7
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-20 20:44:44 +0530

    Added:Weekday Weekend Rate

--- src/HotelReservationMain/HotelReservationTest.java
+++ src/HotelReservationMain/HotelReservationTest.java
@@ -1,18 +1,18 @@
 package HotelReservationMain;
-
 import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Arrays;
 
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 public class HotelReservationTest {
+
     @Test
     public void whenHotelAdded_ToSystem_ShouldGetAdded() {
-        Hotel hotel1 = new Hotel("Lakeewood", 110, 90, 80, 80, 3);
+        Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
         Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
         Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
         Hotel[] hotelList = {hotel1, hotel2, hotel3};
@@ -21,11 +21,12 @@ public class HotelReservationTest {
         hotelReservation.add(hotel1);
         hotelReservation.add(hotel2);
         hotelReservation.add(hotel3);
-        List<Hotel> result = hotelReservation.getHotel();
+        List<Hotel> result = hotelReservation.getHotelList();
         assertEquals(hotels, result);
     }
-    public void whenGivenDateRangeShouldReturnCheapestHotel()
-    {
+
+    @Test
+    public void whenGivenDateRange_ShouldReturn_CheapestHotel() {
         Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
         Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
         Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
@@ -37,6 +38,20 @@ public class HotelReservationTest {
         result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
         assertNotNull(result);
     }
+
+    @Test
+    public void whenGivenHotelAdded_ShouldAddWeekendPrices() {
+        Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.add(hotel1);
+        hotelReservation.add(hotel2);
+        hotelReservation.add(hotel3);
+        List<Hotel> hotelList = hotelReservation.getHotelList();
+        boolean result = hotelList.get(0).getRegularWeekendRate() == 90 &&
+                hotelList.get(1).getRegularWeekendRate() == 60 &&
+                hotelList.get(2).getRegularWeekendRate() == 150;
+        assertTrue(result);
+    }
 }
-
-
